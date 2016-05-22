5ecab49704ddc876d7797fe36859daf2b4c4edf7
Michon van Dooren <michon1992@gmail.com>
2016-05-22 14:26:58 +0200

    Added a method to make it easier to respond_to all udrs formats at once, to DRY things up a bit

--- lib/udrs/engine.rb
+++ lib/udrs/engine.rb
@@ -9,6 +9,10 @@ module UDRS
 
 			Mime::Type.register_alias('application/pdf', :pdf) if !Mime::Type.lookup_by_extension(:pdf)
 			Mime::Type.register_alias('application/octet-stream', :escp) if !Mime::Type.lookup_by_extension(:escp)
+
+			ActionController::MimeResponds::Collector.send(:define_method, 'udrs') do |&block|
+				return any(*%i(pdf escp), &block)
+			end
 		end
 	end
 end
